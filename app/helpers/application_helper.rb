d84eeb9379430534c1b96cc2afdf6eb02ca3c87e
devmuturi <muturinjonge09@gmail.com>
2026-03-18 18:10:48 +0300

    Refactor the flash application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,40 +1,41 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def tailwind_color_class_for(flash_type)
+  def flash_class_for(type)
     {
-      success: "green",
-      error: "red",
-      alert: "orange",
-      notice: "yellow"
-    }[flash_type.to_sym] || flash_type.to_s
+      success: "bg-green-100 border-green-400 text-green-700",
+      error: "bg-red-100 border-red-400 text-red-700",
+      alert: "bg-orange-100 border-orange-400 text-orange-700",
+      notice: "bg-yellow-100 border-yellow-400 text-yellow-700"
+    }[type.to_sym] || "bg-gray-100 border-gray-400 text-gray-700"
   end
 
   def flash_messages(_opts = {})
     flash.each do |msg_type, message|
       next if msg_type == :form_error
 
-      color = tailwind_color_class_for(msg_type)
+      classes = flash_class_for(msg_type)
 
       concat(
         content_tag(
-          :div, class: [
-          "alert",
-          "transition-all",
-          "duration-1000",
-          "ease-in-out",
-          "p-4",
-          "m-4",
-          "mt-5",
-          "w-3/5",
-          "bg-#{color}-100",
-          "border",
-          "border-#{color}-400",
-          "text-#{color}-700",
-          "rounded-lg"
-        ].join(" "), role: msg_type) do
+          :div,
+          class: [
+            "alert",
+            "transition-all",
+            "duration-1000",
+            "ease-in-out",
+            "p-4",
+            "m-4",
+            "mt-5",
+            "w-3/5",
+            classes,
+            "border",
+            "rounded-lg"
+          ].join(" "),
+          role: msg_type
+        ) do
           concat(
-            content_tag(:span, message, class: "block sm:inline") # safe, no html_safe
+            content_tag(:span, message, class: "block sm:inline")
           )
         end
       )
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,40 +4,19 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    return unless user.present? # Guard clause for nil user
+    user ||= User.new # guest user (not logged in)
 
-    if user.has_role?(:admin, user.account)
-      # Admin can do everything
+    if user.has_role?(:user, user.account || Account.new)
+      can [:me, :password, :update_me, :update_password, :standups], User
+      can [:feed, :mine], ActivityController
+      can [:index, :show, :standups], Team
+      cannot :manage, Account
+
+    elsif user.has_role?(:admin, user.account || Account.new) # is an admin
       can :manage, :all
+
     else
-      # Regular user permissions
-
-      # Activity feed
-      can [:mine, :feed], ActivityController
-
-      # User profile
-      can [:password, :update_password, :me, :update_me], User, id: user.id
-
-      # Teams
-      can [:read, :new, :standups], Team, account_id: user.account_id
-      can :create, Team
-      can [:update, :destroy], Team, account_id: user.account_id
-
-      # Team memberships
-      can :manage, TeamMembership, team: { account_id: user.account_id }
-
-      # Days of week
-      can :manage, DaysOfWeekMembership, team: { account_id: user.account_id }
-
-      # Standups
-      can :manage, Standup, user_id: user.id
-      can :read, Standup, team: { account_id: user.account_id }
-
-      # Users in same account
-      can :read, User, account_id: user.account_id
-
-      # Cannot manage other users
-      cannot :manage, User
+      can [:new, :create], Account
     end
   end
 end
